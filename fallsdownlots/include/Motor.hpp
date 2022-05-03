765bea04e6fbba906021867b5ebbb2260c42fd39
gizatt <gregory.izatt@mit.edu>
2022-05-02 21:45:20 -0400

    Stepper motor control!
    Maxes at about 2khz

--- fallsdownlots/include/Motor.hpp
+++ fallsdownlots/include/Motor.hpp
@@ -2,30 +2,146 @@
 
 #include "Arduino.h"
 
-class Motor
+// Phase chart
+const uint8_t stepper_phase_chart[16] = {
+    1, 0, 1, 0,
+    0, 1, 1, 0,
+    0, 1, 0, 1,
+    1, 0, 0, 1};
+
+// Timer interrupt stepping for *all* stepper motors.
+static IntervalTimer stepper_update_timer;
+static bool stepper_update_timer_started;
+static const uint32_t stepper_update_timer_period_us = 100;
+static volatile int n_stepper_motors;
+class StepperMotor;
+static StepperMotor *steppers[2];
+
+class StepperMotor
 {
 public:
-    uint32_t update_period = 1; // ms
+    uint8_t m_en_A;
+    uint8_t m_fwd_A;
+    uint8_t m_rev_A;
+    uint8_t m_en_B;
+    uint8_t m_fwd_B;
+    uint8_t m_rev_B;
+
+    volatile uint8_t m_phase;
+    volatile float m_speed;
+    volatile uint32_t m_stepper_update_timer_last_update;
+    volatile uint32_t m_step_period_us;
+
+    static void do_timer_setup()
+    {
+        n_stepper_motors = 0;
+        stepper_update_timer.begin(advance_steppers, stepper_update_timer_period_us);
+    }
+    static void advance_steppers()
+    {
+        uint32_t t = micros();
+        for (int i = 0; i < n_stepper_motors; i++)
+        {
+            StepperMotor *stepper = steppers[i];
+            uint32_t dt = t - stepper->m_stepper_update_timer_last_update;
+            if (dt > stepper->m_step_period_us)
+            {
+                digitalWrite(stepper->m_fwd_A, stepper_phase_chart[stepper->m_phase * 4 + 0]);
+                digitalWrite(stepper->m_rev_A, stepper_phase_chart[stepper->m_phase * 4 + 1]);
+                digitalWrite(stepper->m_fwd_B, stepper_phase_chart[stepper->m_phase * 4 + 2]);
+                digitalWrite(stepper->m_rev_B, stepper_phase_chart[stepper->m_phase * 4 + 3]);
+                if (stepper->m_speed > 0.)
+                {
+                    stepper->m_phase = (stepper->m_phase + 1) % 4;
+                }
+                else
+                {
+                    // Equivalent to stepping backwards
+                    stepper->m_phase = (stepper->m_phase + 3) % 4;
+                }
+                stepper->m_stepper_update_timer_last_update = t;
+            }
+        }
+    }
+
+    const uint32_t UPDATE_PERIOD = 100; // ms
+    const float MAX_SPEED = 5000.;      // updates / sec
+
+    StepperMotor(uint8_t en_A, uint8_t fwd_A, uint8_t rev_A,
+                 uint8_t en_B, uint8_t fwd_B, uint8_t rev_B) : m_en_A(en_A), m_fwd_A(fwd_A), m_rev_A(rev_A),
+                                                               m_en_B(en_B), m_fwd_B(fwd_B), m_rev_B(rev_B),
+                                                               m_phase(0), m_speed(0.0),
+                                                               m_stepper_update_timer_last_update(micros()), m_step_period_us(1E6),
+                                                               m_enable(1), m_last_update_t(millis())
+    {
+        pinMode(m_en_A, OUTPUT);
+        pinMode(m_en_B, OUTPUT);
+        pinMode(m_fwd_A, OUTPUT);
+        pinMode(m_fwd_B, OUTPUT);
+        pinMode(m_rev_A, OUTPUT);
+        pinMode(m_rev_B, OUTPUT);
+
+        if (!stepper_update_timer_started)
+        {
+            do_timer_setup();
+        }
+
+        steppers[n_stepper_motors] = this;
+        n_stepper_motors += 1;
+    }
+
+    /*
+     Clips to range -1 to 1.
+    */
+    void set_speed(float speed)
+    {
+        noInterrupts();
+        m_speed = max(min(speed, 1.0), -1.0);
+        m_enable = (abs(m_speed) <= 1E-5);
+        m_step_period_us = (uint32_t)max(1., 1E6 / (MAX_SPEED * abs(speed)));
+        interrupts();
+    }
+
+    void update(uint32_t t)
+    {
+        if (t - m_last_update_t > UPDATE_PERIOD)
+        {
+            m_last_update_t = t;
+
+            digitalWrite(m_en_A, !m_enable);
+            digitalWrite(m_en_B, !m_enable);
+        }
+    }
+
+private:
+    bool m_enable;
+    uint32_t m_last_update_t;
+};
+
+class DCMotor
+{
+public:
+    const uint32_t UPDATE_PERIOD = 1; // ms
     uint8_t ANALOG_WRITE_RESOLUTION_BITS = 12;
     uint32_t ANALOG_INPUT_MAX = (1 << ANALOG_WRITE_RESOLUTION_BITS) - 1;
     // Input -DEADBAND -> DEADBAND = 0 input
     // (i.e. remap 0-to-1 to DEADBAND-to-1.)
     float DEADBAND = 0.3;
 
-    Motor(uint8_t pwm_pin, uint8_t fwd_pin, uint8_t rev_pin) : m_pwm_pin(pwm_pin), m_fwd_pin(fwd_pin), m_rev_pin(rev_pin),
-                                                               m_speed(0.0), m_last_update_t(millis())
+    DCMotor(uint8_t en_pin, uint8_t fwd_pin, uint8_t rev_pin) : m_en_pin(en_pin), m_fwd_pin(fwd_pin), m_rev_pin(rev_pin),
+                                                                m_speed(0.0), m_last_update_t(millis())
     {
-        pinMode(m_pwm_pin, OUTPUT);
+        pinMode(m_en_pin, OUTPUT);
         analogWriteResolution(ANALOG_WRITE_RESOLUTION_BITS);
-        // analogWriteFrequency(pwm_pin, 375000);
+        // analogWriteFrequency(en_pin, 375000);
         pinMode(m_fwd_pin, OUTPUT);
         pinMode(m_rev_pin, OUTPUT);
-        write_pins();
+        step_pins();
     }
 
-    void write_pins()
+    void step_pins()
     {
-        analogWrite(m_pwm_pin, (int)(abs(m_speed) * ANALOG_INPUT_MAX));
+        analogWrite(m_en_pin, (int)(abs(m_speed) * ANALOG_INPUT_MAX));
         if (m_speed > 1E-6)
         {
             digitalWrite(m_fwd_pin, 1);
@@ -69,18 +185,17 @@ public:
 
     void update(uint32_t t)
     {
-        if (t - m_last_update_t > update_period)
+        if (t - m_last_update_t > UPDATE_PERIOD)
         {
             m_last_update_t = t;
-            write_pins();
+            step_pins();
         }
     }
 
 private:
-    uint8_t m_pwm_pin;
+    uint8_t m_en_pin;
     uint8_t m_fwd_pin;
     uint8_t m_rev_pin;
     float m_speed;
-    bool m_led_state;
     uint32_t m_last_update_t;
 };
--- fallsdownlots/src/main.cpp
+++ fallsdownlots/src/main.cpp
@@ -20,10 +20,14 @@ LEDBlinker *blinker;
 IMU *imu;
 Display *display;
 
-Motor *motor;
-const uint8_t MOTOR_PWM_PIN = 6;
-const uint8_t MOTOR_FWD_PIN = 7;
-const uint8_t MOTOR_REV_PIN = 8;
+StepperMotor *motor;
+const uint8_t EN_A = 6;
+const uint8_t FWD_A = 7;
+const uint8_t REV_A = 8;
+const uint8_t EN_B = 9;
+const uint8_t FWD_B = 10;
+const uint8_t REV_B = 11;
+
 const uint8_t POT_PIN = 14;
 
 void setup()
@@ -35,7 +39,7 @@ void setup()
   display = new Display(&Serial);
   blinker = new LEDBlinker(LED_BUILTIN);
   imu = new IMU(&Serial);
-  motor = new Motor(MOTOR_PWM_PIN, MOTOR_FWD_PIN, MOTOR_REV_PIN);
+  motor = new StepperMotor(EN_A, FWD_A, REV_A, EN_B, FWD_B, REV_B);
 }
 
 float err_int = 0.;
@@ -50,33 +54,9 @@ void loop()
     display->update(t);
     motor->update(t);
 
-    // float period = 10.0; // seconds
-    // float speed = 0.5 + 0.5 * sin(((float)t) / 1000. * (2. * PI / period));
-    // float speed = ((float)analogRead(POT_PIN)) / 1024.;
-    float angle_zero = -5. + 10. * ((float)analogRead(POT_PIN)) / 1024;
-    float kp = 0.01;
-    float kd = 0.0;
-    float ki = 0.0; //
-    // motor->DEADBAND = ;
-    float angle = imu->angle() * 180. / PI;                    // deg
-    float dangle = -imu->rotational_velocity()[1] * 180. / PI; // deg/s
-    float speed = (angle - angle_zero) * kp + dangle * kd + err_int * ki;
-
-    // Integrate error
-    float dt = max((float)(micros() - last_update_t) / 1E6, 0.);
-    last_update_t = micros();
-    err_int += (angle - angle_zero) * dt;
-    // Anti-windup
-    err_int = max(min(err_int, 1.), -1.);
-
-    if (t < 1000 || abs(angle) > 45) // Don't spin motor on startup or if fallen over.
-    {
-      speed = 0.;
-      // Reset integrator
-      err_int = 0;
-    }
+    float speed = -1. + 2. * (float)analogRead(POT_PIN) / 1024;
 
     motor->set_speed(speed);
-    display->draw_text("A: %.1f\n(%.4f)", angle, angle_zero);
+    display->draw_text("A: %.1f\nS: %u", imu->angle() * 180. / PI, motor->m_step_period_us);
   }
 }
