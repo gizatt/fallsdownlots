eb4803a5b4304382071dabc3fbd5138f6ade1855
gizatt <gregory.izatt@mit.edu>
2022-05-21 19:24:41 -0400

    Try SimpleFOC stepper driver. Doesn't work well at high speeds... badly configured?

--- fallsdownlots/src/main.cpp
+++ fallsdownlots/src/main.cpp
@@ -6,11 +6,11 @@
 #include "snprinth.h"
 
 #include "Arduino.h"
+#include <SimpleFOC.h>
 
 #include "LEDBlinker.hpp"
 #include "IMU.hpp"
 #include "Display.hpp"
-#include "Motor.hpp"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
@@ -20,7 +20,6 @@ LEDBlinker *blinker;
 IMU *imu;
 Display *display;
 
-StepperMotor *motor;
 const uint8_t EN_A = 6;
 const uint8_t FWD_A = 7;
 const uint8_t REV_A = 8;
@@ -28,7 +27,18 @@ const uint8_t EN_B = 9;
 const uint8_t FWD_B = 10;
 const uint8_t REV_B = 11;
 
+// 200 ppr -> 50 pole pairs
+StepperMotor motor = StepperMotor(50, 5.);
+StepperDriver4PWM driver = StepperDriver4PWM(FWD_A, REV_A, FWD_B, REV_B, EN_A, EN_B);
+IntervalTimer foc_update_timer;
+
 const uint8_t POT_PIN = 14;
+
+void timer_update()
+{
+  // Not sure what kind of thread safety implications this has...
+  motor.loopFOC();
+}
 
 void setup()
 {
@@ -39,7 +49,18 @@ void setup()
   display = new Display();
   blinker = new LEDBlinker(LED_BUILTIN);
   imu = new IMU();
-  motor = new StepperMotor(EN_A, FWD_A, REV_A, EN_B, FWD_B, REV_B);
+  // motor = new StepperMotor(EN_A, FWD_A, REV_A, EN_B, FWD_B, REV_B);
+
+  driver.voltage_power_supply = 5;
+  driver.voltage_limit = 5;
+  motor.current_limit = 2.;
+  driver.init();
+  motor.linkDriver(&driver);
+  motor.controller = MotionControlType::velocity_openloop;
+  motor.init();
+  motor.initFOC();
+
+  foc_update_timer.begin(timer_update, 500); // Every 500 us, do an FOC update.
 }
 
 uint32_t last_update_t = 0;
@@ -50,16 +71,15 @@ char print_buf[256];
 float err_int = 0.;
 void loop()
 {
+
   auto t = millis();
 
   // Update all modules.
   blinker->update(t);
   imu->update(t);
   display->update(t);
-  motor->update(t);
 
   // Positive angle is corrected by positive speed.
-
   float angle_target_const = 2; // -5. + 10. * (float)analogRead(POT_PIN) / 1024;
 
   float angle_deg = imu->angle() * 180. / PI;
@@ -69,7 +89,7 @@ void loop()
   float avg_speed = 0.;
 
   //
-  float kp = 0.05;
+  float kp = 0.1;
   float kd = 0.001;
   float ki = 0.;
   float kid = 000.; // avg speed of 0.001 -> 1 deg correction.
@@ -81,7 +101,9 @@ void loop()
     speed = (angle_target - angle_deg) * kp + (0. - dangle_deg) * kd;
   }
 
-  motor->set_speed(speed);
+  // motor->set_speed(speed);
+  // speed = -5. + 10. * (float)analogRead(POT_PIN) / 1024;
+  motor.move(10. * speed);
   avg_speed = avg_speed * AVG_SPEED_ALPHA + (1. - AVG_SPEED_ALPHA) * speed;
 
   // Step odometry
